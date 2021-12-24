ca112290eb9f8ebe74c183d6f4fc67d46145c4e4
Bhavya Sree <bhavyasreekolluri3@gmail.com>
2021-12-24 20:41:19 +0530

    updated

--- Items_to_strings.py
+++ Items_to_strings.py
@@ -3,9 +3,15 @@
 items = ['Hi!' , 'How', 'are','you?']
 x=''.join(items)
 print(x)
-#by using function
+#(OR) by using function
 def combine():
     i = ['o','k']
     j=''.join(i)
     print(j)
 combine()
+#OR
+def list(list1):
+    str = ''
+    print(str.join(list1))
+list1= ['I','am','XYZ']
+list(list1)
