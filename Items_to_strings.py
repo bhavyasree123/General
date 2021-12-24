8b95e6d076b9e2782bb9917cde95a165b5373d8a
Bhavya Sree <bhavyasreekolluri3@gmail.com>
2021-12-24 20:16:20 +0530

    updated

---
+++ Items_to_strings.py
@@ -0,0 +1,11 @@
+#lists into string
+
+items = ['Hi!' , 'How', 'are','you?']
+x=''.join(items)
+print(x)
+#by using function
+def combine():
+    i = ['o','k']
+    j=''.join(i)
+    print(j)
+combine()
