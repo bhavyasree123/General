fe38e808d9dba2354c17ec09fe14f7bcdc4a21a8
Bhavya Sree <bhavyasreekolluri3@gmail.com>
2021-12-25 20:38:35 +0530

    updated

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,8 +1,16 @@
-dup = ['pri','one','two','three','four','five','one','two','ok']
 
+dup = ['one','two','three','four','five','one','two']
 for i in dup:
     print(i)
 if i == i:
     print('It has duplicate elements ',i)
 dup.remove(i)
 print(dup)
+
+#OR 
+li = [1,8,9,0,-7,-7,8,5,1]
+res = []
+for r in li:
+    if r not in res:
+        res.append(r)
+print(res)
