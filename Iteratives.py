73be898f91c51ab81cc9954e5f74483ff530ba2a
Bhavya Sree <bhavyasreekolluri3@gmail.com>
2021-12-26 20:01:31 +0530

    updated

--- Iteratives.py
+++ Iteratives.py
@@ -34,3 +34,22 @@ for i in range(rows,0,-1):
     for j in range(1,i+1):
         print(b,end='')
     print('')
+'''Output   11111
+            2222
+            333
+            44
+            5'''
+    
+rows = 5
+b = 0
+for i in range(rows,0,-1):
+    b = +1
+    for j in range(1,i+1):
+        print(b,end='')
+    print('')
+
+'''Output   11111
+            1111
+            111
+            11
+            1'''
