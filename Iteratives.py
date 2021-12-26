3335773e0ac7ef90a05617df4a24d3167f65dae0
Bhavya Sree <bhavyasreekolluri3@gmail.com>
2021-12-26 19:58:24 +0530

    updated

--- Iteratives.py
+++ Iteratives.py
@@ -26,3 +26,11 @@ for i in range(1, rows + 1):
             1 2 3
             1 2 3 4
             1 2 3 4 5'''
+
+rows = 5
+b = 0
+for i in range(rows,0,-1):
+    b += 1
+    for j in range(1,i+1):
+        print(b,end='')
+    print('')
