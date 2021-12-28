a3c67e48666165d68c2fed58de59e0e2890aade6
Bhavya Sree <bhavyasreekolluri3@gmail.com>
2021-12-28 14:52:35 +0530

    updated

---
+++ even_length_words.py
@@ -0,0 +1,21 @@
+
+s = "I am Bhavya Sree " 
+s = s.split()
+for w in s:
+    if len(s)%2 == 0: #it prints the words only if the 's' has the even number of words in a sentence
+        print(w)
+'''Output I
+        am
+        Bhavya
+        Sree '''
+
+s = 'I am Bhavya Sree CSE'
+s = s.split()
+for w in s:
+    if len(w)%2==0: #it prints only if the single word inside the sentence has even alphabets
+        print(w)
+    #else:
+        #print("It don't have even alphabets in a word")
+''' Outpt :am
+        Bhavya
+        Sree'''
--- working_with_time.py
+++ working_with_time.py
@@ -5,5 +5,6 @@ y = time.ctime()
 print(y) #gives the current time like this Tue Dec 28 13:40:19 2021
 z = time.localtime()#time.struct_time(tm_year=2021, tm_mon=12, tm_mday=28, tm_hour=13, tm_min=41, tm_sec=35, tm_wday=1, tm_yday=362, tm_isdst=0)
 print(z)
-s = time.sleep(3) #the sleep function will run the next following statement after 5 seconds which has given as parameter
+s = time.sleep(0) #the sleep function will run the next following statement after 5 seconds which has given as parameter
 print('Hello There!')
+
