f3a95310a05f11fc10ea0608603b3bf55150340f
Bhavya Sree <bhavyasreekolluri3@gmail.com>
2022-01-12 22:20:20 +0530

    updated

--- check_fibonacci_or_not.py
+++ check_fibonacci_or_not.py
@@ -1,2 +1,15 @@
-n =  int(input("Enter your number"))
+n =  int(input("Enter your number : "))
 c = 0
+a = 0
+b = 1
+if n==0 or n==1:
+    print("YES")
+else:
+    while c<n:
+        c = a+b
+        a =b
+        b=c
+    if c == n:
+        print("YES")
+    else:
+        print("NO :<(")
