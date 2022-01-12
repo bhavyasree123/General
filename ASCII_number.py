80b4f30a2f8aa9562d77bbbdbd5110dcf9b4b4e5
Bhavya Sree <bhavyasreekolluri3@gmail.com>
2022-01-12 22:37:41 +0530

    updated

--- ASCII_number.py
+++ ASCII_number.py
@@ -1,3 +1,6 @@
 # Program to find the ASCII value of the given character
 character = input("Enter your character: ")
 print("The ascii value of ",character, "is" , ord(character))
+
+#ASCII - American Standard Code for Information Interchange is the unique code given to the characters or elements and numbers on the keyboard
+#It is a numeric value given to different characters and symbols, for computers to store and manipulate. 
