85fee22a386a98458b608c1f2452e8aaba813b50
Bhavya Sree <bhavyasreekolluri3@gmail.com>
2022-01-18 20:53:54 +0530

    updated

--- b.py
+++ b.py
@@ -9,3 +9,13 @@ print(k)
 sp = "ahbxa jgh jhj sbxa"
 print(sp.split())
 print(s[0:2])
+
+input_str = int(input())
+arr = []
+print("Enter the elements: ")
+for i in range(input_str):
+    element = int(input())
+    arr.append(element)
+    rev = arr[0]-arr[-1]
+print(rev)
+print("hello")
---
+++ perfect_in_array.py
@@ -0,0 +1,4 @@
+arr = [1,4,9,8,10]
+for i in range(1,arr):
+    arr%i == 0
+    print(arr)
